2066f84e0851ad512659e91a61bd0693341d927b
yuichiyasui <yassan.b.a.w@gmail.com>
2023-10-28 14:59:48 +0900

    Remove unnecesary codes from main.ts

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,29 @@
 import { createServer } from "node:http";
 import path from "node:path";
 
-import cookieParser from "cookie-parser";
-import express, { ErrorRequestHandler } from "express";
-import createError from "http-errors";
+import express from "express";
+import createHttpError from "http-errors";
 import logger from "morgan";
 import { Server } from "socket.io";
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
-
-// catch 404 and forward to error handler
-app.use(function (_, __, next) {
-  next(createError(404));
-});
-
-const errorHandler: ErrorRequestHandler = (err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-};
-
-app.use(errorHandler);
 
 app.get("/", (_, res) => {
   res.sendFile("index.html");
 });
+
+app.all("*", (_, res) => {
+  const error = createHttpError(404);
+  res
+    .status(error.status)
+    .sendFile(path.join(__dirname, "../public/not-found.html"));
+});
+
+const server = createServer(app);
+const io = new Server(server);
 
 io.on("connection", (socket) => {
   socket.on("chat1", (msg) => {
