9b579460b96b4447f2c917b28a8633a13a27ba44
yuichiyasui <yassan.b.a.w@gmail.com>
2023-10-28 09:06:58 +0900

    Fix app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,4 +36,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port} 🚀`);
+});
